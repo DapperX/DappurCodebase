571a8c0b8f1e07bfe62711dd909b74918b881d8b
Dappur <dapperx@126.com>
2020-04-24 19:41:59 +0800

    Update `wrapper`
    wrapper: adjust code style

--- wrapper.hpp
+++ wrapper.hpp
@@ -16,25 +16,25 @@ public:
 template<auto F>
 class wrapper_function
 {
-    template<typename> class inner;
+	template<typename> class inner;
 
-    template<class Cls, typename Ret, typename... Args>
-    class inner<Ret(Cls::*)(Args...)>
-    {
-    public:
-        using type_func = Ret(Args...);
+	template<class Cls, typename Ret, typename... Args>
+	class inner<Ret(Cls::*)(Args...)>
+	{
+	public:
+		using type_func = Ret(Args...);
 
-        static auto bind(Cls *obj)
-        {
-            return [=](Args ...args){
-                return (obj->*F)(std::forward<Args>(args)...);
-            };
-        }
-    };
+		static auto bind(Cls *obj)
+		{
+			return [=](Args ...args){
+				return (obj->*F)(std::forward<Args>(args)...);
+			};
+		}
+	};
 
 public:
-    using type_func = typename inner<decltype(F)>::type_func;
-    static const constexpr auto bind = inner<decltype(F)>::bind;
+	using type_func = typename inner<decltype(F)>::type_func;
+	static const constexpr auto bind = inner<decltype(F)>::bind;
 };
 
 
